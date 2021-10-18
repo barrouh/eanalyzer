398f81dfbe0168739a9dd7d0c11b6183c30f16f3
Mohamed Barrouh <mohamed.barrouh@gmail.com>
2021-10-18 14:27:54 +0200

    init

---
+++ eanalyzer-backend/src/main/java/io/eanalyzer/main/Eanalyzer.java
@@ -0,0 +1,13 @@
+package io.eanalyzer.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eanalyzer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eanalyzer.class, args);
+	}
+
+}
---
+++ setPathNodeJs.bat
@@ -0,0 +1,3 @@
+@cd C:\development\E_A\eanalyzer\eanalyzer-frontend
+@set PATH=C:\tools\node-v14.16.1;%PATH%
+@cmd.exe /K
